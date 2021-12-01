1018f5b2b9107e208fb2d87ca70e8fd1dbcf26ff
Greg Hull <greg.hull@gmail.com>
2021-12-01 09:30:09 -0500

    Use the built-in reader

--- src/advent_2021/day_1.clj
+++ src/advent_2021/day_1.clj
@@ -1,13 +1,12 @@
-(ns advent-2021.day-1
-  (:require [clojure.string :as str]))
+(ns advent-2021.day-1)
 
-(defn input
+(defn read-seq
   "Read in a file with a list of integers into a sequence"
    [fname]
   (->> fname
        slurp
-       str/split-lines
-       (map #(Integer/parseInt %))))
+       (#(str "[" % "]"))
+       read-string))
 
 (defn increases
   "Given a sequence `v` of n numbers, returns list of n-1 booleans
@@ -22,21 +21,21 @@
 (defn sliding-windows
   ([v] (sliding-windows v []))
   ([v wins]
-    (let [[a b c] v]
-      (if (nil? c)
-        wins
-        (recur (rest v) (conj wins (+ a b c)))))))
+   (let [[a b c] v]
+     (if (nil? c)
+       wins
+       (recur (rest v) (conj wins (+ a b c)))))))
 
 ;; Part 1
 (->> "resources/day_1.txt"
-     input
+     read-seq
      increases
      (filter true?)
      count)
 
 ;; Part 2
 (->> "resources/day_1.txt"
-     input
+     read-seq
      sliding-windows
      increases
      (filter true?)
