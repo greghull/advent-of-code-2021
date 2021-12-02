e9dd9f2ed16c62dcdce61192e52eb4687687fac1
Greg Hull <greg.hull@gmail.com>
2021-12-02 09:23:25 -0500

    got rid of crap

--- src/advent_2021/day_1.clj
+++ src/advent_2021/day_1.clj
@@ -5,38 +5,17 @@
    [f]
   (->> f slurp (#(str "[" % "]")) read-string))
 
-(defn increases
-  "Given a collection of n numbers, returns list of n-1 booleans
-  determined by v[i] < v[i+1]"
-  ([coll] (increases coll []))
-  ([coll results]
-   (let [[a b] coll]
-     (if (nil? b)
-       results
-       (recur (rest coll) (conj results (< a b)))))))
-
-
-(defn sliding-windows
-  "Returns a sequence of the sums of `n` element windows from the
-  collection `coll`"
-  ([n coll] (sliding-windows n coll []))
-  ([n coll wins]
-   (let [window (take n coll)]
-     (if (> n (count window))
-       wins
-       (recur n (rest coll) (conj wins (reduce + window)))))))
-
 ;; Part 1
 (->> "resources/day_1.txt"
-     read-seq
-     increases
-     (filter true?)
-     count)
+     read-seq                                               ;convert text to seq
+     (#(map < % (rest %)))                                  ;map to s_i < s_i+1
+     (filter true?)                                         ;filter out false
+     count)                                                 ;count results
 
 ;; Part 2
 (->> "resources/day_1.txt"
-     read-seq
-     (sliding-windows 3)
-     increases
-     (filter true?)
-     count)
+     read-seq                                               ;convert text to seq
+     (#(map + % (rest %) (nthrest % 2)))                    ;map to s_i + s_i+1 + s+i+2
+     (#(map < % (rest %)))                                  ;map to s_i < s_i+1
+     (filter true?)                                         ;filter out false
+     count)                                                 ;count the results
