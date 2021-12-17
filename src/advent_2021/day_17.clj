94708abac6a0f2e53a0afde6cbc7b7e67fef0833
Greg Hull <greg.hull@gmail.com>
2021-12-17 08:41:01 -0500

    cleanup

--- src/advent_2021/day_17.clj
+++ src/advent_2021/day_17.clj
@@ -1,6 +1,5 @@
 (ns advent-2021.day-17)
 
-;target area: x=185..221, y=-122..-74
 (def target {:x1 185 :x2 221 :y1 -122 :y2 -74})
 
 (defn drag-x [vx]
@@ -9,30 +8,26 @@
     (< vx 0) (inc vx)
     (= vx 0) 0))
 
+(defn hit? [target x y]
+  (and (>= (:x2 target ) x (:x1 target)) (>= (:y2 target) y (:y1 target))))
+
 (defn fire [target [init_vx init_vy]]
-  (loop [x 0
-         y 0
-         max_y 0
-         vx init_vx
-         vy init_vy]
-    (cond (< y (:y1 target)) nil
-          (and (>= x (:x1 target)) (<= x (:x2 target))
-            (>= y (:y1 target)) (<= y (:y2 target)))
-          {:x x :y y :vx vx :vy vy :max_y max_y :init_vx init_vx :init_vy init_vy}
+  (loop [x 0 y 0 max_y 0 vx init_vx vy init_vy]
+    (cond (< y (:y1 target)) nil  ; we missed the target range
+          (hit? target x y) max_y ; hit, return the max height
           :else (recur (+ x vx) (+ y vy) (max max_y y) (drag-x vx) (dec vy)))))
 
 ; Part 1
-(->> (for [x (range 0 300)
-           y (range 0 300)]
-       [x y])                     ; generate sequence of initial values
+(->> ; generate sequence of initial velocities
+  (for [x (range 0 300) y (range 0 300)] [x y])
   (pmap #(fire target %))         ; map them to firing outcome
   (filter identity)               ; filter out the misses
-  (sort-by :max_y >)              ; sort by max height
+  (sort >)                        ; sort by max height
   first)                          ; first one is the answer
 
 ; Part 2
-(->> (for [x (range 0 500) y (range -300 300)]
-       [x y])                     ; generate sequence of initial values
+(->> ; generate sequence of initial velocities
+  (for [x (range 0 300) y (range -300 300)] [x y])
   (pmap #(fire target %))         ; map them to firing outcome
   (filter identity)               ; filter out the misses
   count)                          ; count the results
